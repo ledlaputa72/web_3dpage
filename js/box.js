cd22c56b50231456b7766893f97a1263d483da55
ledlaputa <ledlaputa@gmail.com>
2020-12-22 13:22:37 +0900

    ss

---
+++ js/box.js
@@ -0,0 +1,35 @@
+const box = document.getElementById('box');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+const reset = document.getElementById('reset');
+let xDeg = 0;
+let angleX = 0;
+const hammer = new Hammer(box);
+
+/* ------------------------------------------------ */
+
+left.addEventListener('click', function(){
+    angleX += 10;
+    box.style.transform = 'rotate(' + angleX + 'deg)';
+});
+
+right.addEventListener('click', function(){
+    angleX -= 10;
+    box.style.transform = 'rotate(' + angleX + 'deg)';
+});
+
+reset.addEventListener('click', function(){
+    angleX = 0;
+    box.style.transform = 'rotate(' + angleX + 'deg)';
+});
+
+/* ------------------------------------------------- */
+hammer.on('panleft', function(){
+    angle += 10;
+	box.style.transform= 'rotate(' + angle + 'deg)';
+});
+
+hammer.on('panright', function(){
+    angle -= 10;
+	box.style.transform= 'rotate(' + angle + 'deg)';
+});
